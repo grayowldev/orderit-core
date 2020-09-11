761fb580050eb1a08a8b3d01a23c2a9a76435502
grayowldev <truegrayowl@gmail.com>
2020-09-11 17:22:21 -0400

    working on Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,35 @@
-public class Customer {
+
+public class Customer implements User{
+    String id,name;
+    Address address;
+
+
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public String generateId() {
+        return null;
+    }
+
+    @Override
+    public String setId(String id) {
+        this.id =id;
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+
+    @Override
+    public Address getAddress() {
+        return this.address;
+    }
+
 }
